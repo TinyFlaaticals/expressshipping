5b76fdba212d8c54c10a715f97bf25705d14df7e
TinyFlaaticals <illustratedmaldivesmv@gmail.com>
2025-01-16 19:24:33 +0500

    feat: add Global Partners button to main page

---
+++ src/pages/NewPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+const NewPage = () => {
+  return (
+    <div className="page-container">
+      <Header />
+      <main className="main-content">
+        {/* Your new page content will go here */}
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default NewPage; 
