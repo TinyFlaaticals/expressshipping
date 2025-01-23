0f36f0f21dec6dd54b2cc819f4892c00d9fc3fad
TinyFlaaticals <illustratedmaldivesmv@gmail.com>
2025-01-24 01:59:34 +0500

    commit

--- next.config.js
+++ next.config.js
@@ -3,6 +3,21 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        has: [
+          {
+            type: 'host',
+            value: 'www.expressshipping.mv',
+          },
+        ],
+        destination: 'https://expressshipping.mv',
+        permanent: true,
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig 
