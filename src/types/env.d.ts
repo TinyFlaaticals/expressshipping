6c23b7f0af48493cbea008d07b2628287dec0af4
TinyFlaaticals <illustratedmaldivesmv@gmail.com>
2025-01-26 04:06:01 +0500

    Update env.d.ts
    commit

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -3,8 +3,6 @@ declare global {
     interface ProcessEnv {
       RESEND_API_KEY: string;
       RECIPIENT_EMAIL: string;
-      NODE_ENV: 'development' | 'production';
-      // Add any other environment variables here
     }
   }
 }
